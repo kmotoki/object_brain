ba4a67836902ffb1760f8a37748029bb05bc4486
Kiyoshi Motoki <kiyoshi.motoki@gmail.com>
2020-10-22 23:45:08 +0900

    Implement 3.4

---
+++ 03/3.4/bucho.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+require_relative './shain.rb'
+
+# Bucho class inheritd from Shain class for lesson 3.4
+class Bucho < Shain
+	def standup
+		puts '部長がだるそうに立ちました。'
+	end
+end
