fa53e95e7d015aa0408b8b44c07774a0e2ad54eb
Kiyoshi Motoki <kiyoshi.motoki@gmail.com>
2020-11-01 22:32:54 +0900

    Implement 3.7

---
+++ 03/3.7/torishimariyaku.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+require_relative './shain.rb'
+
+class Torishimariyaku < Shain
+	def calculate_salary(kihonkyu)
+		kihonkyu * 4 + 2
+	end
+
+	def standup
+		'ふんぞり返って立ち上がりました。'
+	end
+end
